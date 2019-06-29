e77967e3ac0954eb35e994be563e764a6374c7ec
txc <1273586908@qq.com>
2019-06-29 11:22:19 +0800

    s

--- src/app/confession/collect-zan/collect-zan.component.ts
+++ src/app/confession/collect-zan/collect-zan.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var BizQQWPA: any;
+
 @Component({
   selector: 'app-collect-zan',
   templateUrl: './collect-zan.component.html',
@@ -21,6 +23,7 @@ export class CollectZanComponent implements OnInit {
     }else {
       this.seeScope = 2;
     }
+    BizQQWPA.addCustom({aty: '0', a: '0', nameAccount: 800098263, selector: 'qq'});
   }
 
 }
